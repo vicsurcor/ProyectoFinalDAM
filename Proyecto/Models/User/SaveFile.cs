85ced4fb135ad765e25f33a6a4461fd019a26d45
v.suros <v.suros@i3code.es>
2025-04-03 15:24:19 +0200

    ProyectoFinal-Api-AddedSaveModel

---
+++ Proyecto/Models/User/SaveFile.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto.Models.User
+{
+    public class SaveFile
+    {
+        public int Id { get; set; } = 1;
+        public string Name { get; set; } = "TestSave.Json";
+        public string Content { get; set; } = "";
+        public DateTime SaveTime { get; set; } = DateTime.Now;
+
+    }
+}
