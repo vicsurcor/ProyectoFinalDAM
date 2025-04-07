0101b6fa22b25a7b1ebf4251c24942f6153dd7ab
v.suros <v.suros@i3code.es>
2025-04-07 14:45:39 +0200

    ProyectoFinal-Api-UserCreation/Saving/BasicAuthentication

--- Proyecto/Controllers/AccountController.cs
+++ Proyecto/Controllers/AccountController.cs
@@ -1,18 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Proyecto.Models.User;
+using Proyecto.Extensions;
 
 namespace Proyecto.Controllers
 {
     [Route("[controller]")]
     public class AccountController : Controller
     {
+        #region Select
         [HttpGet("[action]")]
         public IActionResult Login()
-        {
-            return View();
-        }
-        [HttpPost("[action]")]
-        public IActionResult Login(User user)
         {
             return View();
         }
@@ -21,10 +19,90 @@ namespace Proyecto.Controllers
         {
             return View();
         }
+        #endregion
+
+        #region Insert
+        [HttpPost("[action]")]
+        public IActionResult Login(User user)
+        {
+            // Path to the JSON file
+            string filePath = "TestData/DataStartUsers.json";
+
+            // Read the JSON file
+            string json = "";
+            try
+            {
+                using StreamReader reader = new StreamReader(filePath);
+                json = reader.ReadToEnd();
+                Console.WriteLine("File read successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            /*File.ReadAllText(filePath);*/
+
+            // Deserialize the JSON to a list of users
+            List<UserContent> users = JsonConvert.DeserializeObject<List<UserContent>>(json);
+
+            // Check if the input user matches any user in the list
+            foreach (var _user in users)
+            {
+                if (_user.User.UserName == user.UserName && _user.User.Password == user.Password)
+                {
+                    UserViewModel view = new UserViewModel(_user.User, _user);
+                    HttpContext.Session.SetObject("UserModel", view);
+                    return RedirectToAction("Index", "Stats");
+                }
+            }
+            return View();
+        }
         [HttpPost("[action]")]
         public IActionResult Register(User user)
         {
+            // Path to the JSON file
+            string filePath = "TestData/DataStartUsers.json";
+
+            // Read the JSON file
+            string json = "";
+            try
+            {
+                using StreamReader reader = new StreamReader(filePath);
+                json = reader.ReadToEnd();
+                Console.WriteLine("File read successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            /*File.ReadAllText(filePath);*/
+
+            // Deserialize the JSON to a list of users
+            List<UserContent> users = JsonConvert.DeserializeObject<List<UserContent>>(json);
+
+            // Check if the input user matches any user in the list
+            foreach (var _user in users)
+            {
+                if (_user.User.UserName == user.UserName )
+                {
+                    return View();
+                }
+                else
+                {
+                    users.Add(new UserContent(user));
+                    json = JsonConvert.SerializeObject(users, Formatting.Indented);
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        writer.Write(json);
+                        Console.WriteLine("File overwritten successfully.");
+                    }
+                    return View("Login");
+                }
+            }
+            
             return View();
+
         }
+        #endregion
     }
 }
--- Proyecto/Controllers/StatsController.cs
+++ Proyecto/Controllers/StatsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Proyecto.Models.User;
+using Proyecto.Extensions;
 
 namespace Proyecto.Controllers
 {
@@ -9,18 +10,20 @@ namespace Proyecto.Controllers
         [HttpGet("[action]")]
         public IActionResult Index()
         {
+            var userModel = HttpContext.Session.GetObject<UserViewModel>("UserModel");
             ViewBag.SecondaryLayout = "_StatsLayout";
             ViewBag.CurrentView = "Stats_Index";
             ViewBag.SecView = "Stats_Index";
-            return View(/*Object for Tests*/ new UserViewModel());
+            return View(/*Object for Tests new UserViewModel()*/userModel);
         }
         [HttpGet("[action]")]
         public IActionResult Saves()
         {
+            var userModel = HttpContext.Session.GetObject<UserViewModel>("UserModel");
             ViewBag.SecondaryLayout = "_StatsLayout";
             ViewBag.CurrentView = "Stats_Index";
             ViewBag.SecView = "Stats_Saves";
-            return View(/*Object for Tests*/ new UserContent());
+            return View(/*Object for Tests new UserContent()*/ userModel.UserContent);
         }
     }
 }
