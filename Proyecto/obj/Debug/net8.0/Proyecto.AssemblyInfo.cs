eca1c823f696753ad65a073c1e4cfa1480b6d213
v.suros <v.suros@i3code.es>
2025-04-08 15:05:05 +0200

    ///

--- Proyecto/obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
+++ Proyecto/obj/Debug/net8.0/Proyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc040bc3bd15d13c5bf892fd389ad9e9b5241978")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83982ff6ed946c0f0a4c036ac0f0b52704cdab37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
