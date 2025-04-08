b8f2d22e430d1d72bf16e9cab02069660f9da75e
v.suros <v.suros@i3code.es>
2025-04-08 15:04:46 +0200

    ProyectoFinal-Api-ExtractedExtraMethods

---
+++ Proyecto/Extra/InitializeId.cs
@@ -0,0 +1,16 @@
+﻿using Proyecto.Models.User;
+
+namespace Proyecto.Extra
+{
+    public static class InitializeId
+    {
+        
+        public static int InitializeIds()
+        {
+            
+            List<UserContent> users = JsonMethods.GetJsonUserContents();
+            return users.Count;
+        }
+    }
+        
+}
---
+++ Proyecto/Extra/JsonMethods.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Proyecto.Models.User;
+
+namespace Proyecto.Extra
+{
+    public class JsonMethods
+    {
+        // Path to the JSON file
+        static string filePath = "TestData/DataStartUsers.json";
+        public static List<UserContent> GetJsonUserContents()
+        {
+            // Read the JSON file
+            string json = "";
+            try
+            {
+                using StreamReader reader = new StreamReader(filePath);
+                json = reader.ReadToEnd();
+                Console.WriteLine("File read successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+            /*File.ReadAllText(filePath);*/
+
+            // Deserialize the JSON to a list of users
+            List<UserContent> users = JsonConvert.DeserializeObject<List<UserContent>>(json);
+            return users;
+        }
+        public static void UpdateJsonUserContents(List<UserContent> users)
+        {
+            string json = JsonConvert.SerializeObject(users, Formatting.Indented);
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.Write(json);
+                Console.WriteLine("File overwritten successfully.");
+            }
+        }
+    }
+}
