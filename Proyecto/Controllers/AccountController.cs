83fc71e71905c7c1540cd35b64997cb7352d89d5
v.suros <v.suros@i3code.es>
2025-04-08 15:04:22 +0200

    ProyectoFinal-Api-SimplifiedJsonCalls

--- Proyecto/Controllers/AccountController.cs
+++ Proyecto/Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Newtonsoft.Json;
 using Proyecto.Models.User;
 using Proyecto.Extensions;
+using Proyecto.Extra;
 
 namespace Proyecto.Controllers
 {
@@ -25,25 +26,8 @@ namespace Proyecto.Controllers
         [HttpPost("[action]")]
         public IActionResult Login(User user)
         {
-            // Path to the JSON file
-            string filePath = "TestData/DataStartUsers.json";
-
-            // Read the JSON file
-            string json = "";
-            try
-            {
-                using StreamReader reader = new StreamReader(filePath);
-                json = reader.ReadToEnd();
-                Console.WriteLine("File read successfully.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
-            /*File.ReadAllText(filePath);*/
-
             // Deserialize the JSON to a list of users
-            List<UserContent> users = JsonConvert.DeserializeObject<List<UserContent>>(json);
+            List<UserContent> users = JsonMethods.GetJsonUserContents();
 
             // Check if the input user matches any user in the list
             foreach (var _user in users)
@@ -60,49 +44,29 @@ namespace Proyecto.Controllers
         [HttpPost("[action]")]
         public IActionResult Register(User user)
         {
-            // Path to the JSON file
-            string filePath = "TestData/DataStartUsers.json";
-
-            // Read the JSON file
-            string json = "";
-            try
-            {
-                using StreamReader reader = new StreamReader(filePath);
-                json = reader.ReadToEnd();
-                Console.WriteLine("File read successfully.");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred: {ex.Message}");
-            }
-            /*File.ReadAllText(filePath);*/
 
             // Deserialize the JSON to a list of users
-            List<UserContent> users = JsonConvert.DeserializeObject<List<UserContent>>(json);
+            List<UserContent> users = JsonMethods.GetJsonUserContents();
 
             // Check if the input user matches any user in the list
             foreach (var _user in users)
             {
-                if (_user.User.UserName == user.UserName )
+                if (_user.User.UserName == user.UserName)
                 {
                     return View();
                 }
                 else
                 {
                     users.Add(new UserContent(user));
-                    json = JsonConvert.SerializeObject(users, Formatting.Indented);
-                    using (StreamWriter writer = new StreamWriter(filePath))
-                    {
-                        writer.Write(json);
-                        Console.WriteLine("File overwritten successfully.");
-                    }
-                    return View("Login");
+                    JsonMethods.UpdateJsonUserContents(users);
+                    return RedirectToAction("Login", "Account");
                 }
             }
             
             return View();
 
         }
+
         #endregion
     }
 }
